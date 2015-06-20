8ff67b5a65177ef49b75fc4c46e5a1034d59afbb
Takeru Komiya <tkomiya24@gmail.com>
2015-06-20 09:05:36 -0400

    Adds a test case class for testing the VocabListJsonConverter

---
+++ tests/com/tkomiya/tests/VocabListJsonConverterTest.java
@@ -0,0 +1,45 @@
+package com.tkomiya.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.tkomiya.infrastructure.VocabListJsonConverter;
+import com.tkomiya.models.VocabList;
+import com.tkomiya.testutils.Constants;
+import com.tkomiya.testutils.TestUtils;
+
+public class VocabListJsonConverterTest {
+
+	public static final String JSON_FILEPATH = Constants.TEST_DIRECTORY + "vocablist.json";
+	
+	@Test
+	public void testReadJson() throws UnsupportedEncodingException, FileNotFoundException, JSONException {
+		//Arrange
+		JSONObject vocabListJson = new JSONObject(TestUtils.readFile(JSON_FILEPATH));
+		VocabList expectedList = TestVocabListLoader.makeTestList();
+		//Act
+		VocabList vocabList = VocabListJsonConverter.convertJsonToVocabList(vocabListJson);
+		//Assert
+		assertEquals(expectedList, vocabList);
+	}
+	
+	@Test
+	public void testMakeJson() throws UnsupportedEncodingException, FileNotFoundException, JSONException {
+		//Arrange
+		VocabList testList = TestVocabListLoader.makeTestList();
+//		JSONObject expectedJson = new JSONObject(TestUtils.readFile(JSON_FILEPATH));
+		//Act
+		JSONObject actualJson = VocabListJsonConverter.convertVocabListToJson(testList);
+		System.out.println(actualJson.toString());
+		//Assert
+		assertEquals(testList, VocabListJsonConverter.convertJsonToVocabList(actualJson));
+	}
+	
+}
