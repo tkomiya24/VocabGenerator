77a1437aa662d5ac8cec14cca8f32fc7e5d66ffd
Takeru Komiya <tkomiya24@gmail.com>
2015-06-20 09:06:56 -0400

    Adds a method to read the contents of a file as a string

--- src/com/tkomiya/infrastructure/FileUtilities.java
+++ src/com/tkomiya/infrastructure/FileUtilities.java
@@ -1,6 +1,12 @@
 package com.tkomiya.infrastructure;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
 
 import com.tkomiya.main.MainController;
 
@@ -39,6 +45,16 @@ public class FileUtilities {
 		else{
 			return getFilePathWithoutFileName(file) + getFileNameWithNoExtension(file) + "." + MainController.VOCAB_LIST_FILE_EXTENSION;
 		}
+	}
+	
+	public static String readFile(String filePath) throws UnsupportedEncodingException, FileNotFoundException {
+		Scanner scan = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath)), "UTF-8")));
+		String jsonString = new String();
+		while (scan.hasNextLine()) {
+			jsonString = jsonString.concat(scan.nextLine());
+		}
+		scan.close();
+		return jsonString;
 	}
 	
 }
