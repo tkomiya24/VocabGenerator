9bae8fc71392a4f6e9acaf249f6c723bda20230b
Takeru Komiya <tkomiya24@gmail.com>
2015-07-19 08:23:20 -0400

    Fixes the MostMistakenDescendingVocabComparator to use the new languages enum

--- src/com/tkomiya/infrastructure/MostMistakenDescendingVocabComparator.java
+++ src/com/tkomiya/infrastructure/MostMistakenDescendingVocabComparator.java
@@ -3,19 +3,18 @@ package com.tkomiya.infrastructure;
 import java.util.Comparator;
 
 import com.tkomiya.models.Vocab;
+import com.tkomiya.models.Vocab.SupportedLanguage;
 
-public class MostMistakenDescendingVocabComparator implements Comparator {
+public class MostMistakenDescendingVocabComparator implements Comparator<Vocab> {
 
-	private int language;
+	private SupportedLanguage language;
 	
-	public MostMistakenDescendingVocabComparator(int language) {
+	public MostMistakenDescendingVocabComparator(SupportedLanguage language) {
 		this.language = language;
 	}
 	
 	@Override
-	public int compare(Object arg0, Object arg1) {
-		Vocab vocab1 = (Vocab) arg0;
-		Vocab vocab2 = (Vocab) arg1;
+	public int compare(Vocab vocab1, Vocab vocab2) {
 		float successRate1 = vocab2.getTimesTested(language) == Integer.MAX_VALUE ? 0 : (float) vocab2.getTimesCorrect(language) / vocab2.getTimesTested(language);
 		float successRate0 = vocab1.getTimesTested(language) == Integer.MAX_VALUE ? 0 : (float) vocab1.getTimesCorrect(language) / vocab1.getTimesTested(language);
 		if (successRate1 > successRate0) {
