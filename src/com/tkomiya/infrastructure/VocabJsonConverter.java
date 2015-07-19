36faab9c8b0c8f58097edacc0fe0dc0657b0c2de
Takeru Komiya <tkomiya24@gmail.com>
2015-07-19 10:14:34 -0400

    Methods now throw exception instead of catching it

--- src/com/tkomiya/infrastructure/VocabJsonConverter.java
+++ src/com/tkomiya/infrastructure/VocabJsonConverter.java
@@ -1,7 +1,5 @@
 package com.tkomiya.infrastructure;
 
-import java.util.Arrays;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -15,43 +13,33 @@ public class VocabJsonConverter {
 	public static final String TIMES_CORRECT = "Times Correct";
 	public static final String TRANSLATION = "Translation";
 	
-	public static JSONObject convertToJson(Vocab vocab) {
+	public static JSONObject convertToJson(Vocab vocab) throws JSONException {
 		JSONObject vocabJson = new JSONObject();
-		try {
-			vocabJson.put(PRIMARY_LANGUAGE, vocab.getPrimaryLanguage());
-			for (SupportedLanguage language : SupportedLanguage.values()) {
-				if (vocab.getTranslation(language) != null) {
-					JSONObject languageJson = new JSONObject();
-					languageJson.put(TRANSLATION, vocab.getTranslation(language));
-					languageJson.put(TIMES_CORRECT, vocab.getTimesCorrect(language));
-					languageJson.put(TIMES_TESTED, vocab.getTimesTested(language));
-					vocabJson.put(language.toString(), languageJson);
-				}
+		vocabJson.put(PRIMARY_LANGUAGE, vocab.getPrimaryLanguage());
+		for (SupportedLanguage language : SupportedLanguage.values()) {
+			if (vocab.getTranslation(language) != null) {
+				JSONObject languageJson = new JSONObject();
+				languageJson.put(TRANSLATION, vocab.getTranslation(language));
+				languageJson.put(TIMES_CORRECT, vocab.getTimesCorrect(language));
+				languageJson.put(TIMES_TESTED, vocab.getTimesTested(language));
+				vocabJson.put(language.toString(), languageJson);
 			}
-			return vocabJson;  
-		} catch (JSONException e) {
-			e.printStackTrace();
-			return null;
 		}
+		return vocabJson;  
 	}
 	
-	public static Vocab convertToVocab(JSONObject json) {
-		try {
-			String primaryLanguageString = json.getString(PRIMARY_LANGUAGE);
-			SupportedLanguage primaryLanguage = SupportedLanguage.valueOf(primaryLanguageString.toUpperCase().trim());
-			Vocab vocab = new Vocab(primaryLanguage);
-			for (SupportedLanguage language : SupportedLanguage.values()) {
-				if (json.has(language.toString())) {
-					JSONObject langJson = json.getJSONObject(language.toString());
-					vocab.addLanguage(language, langJson.getString(TRANSLATION));
-					vocab.setScore(language, langJson.getInt(TIMES_TESTED), langJson.getInt(TIMES_CORRECT));
-				}
+	public static Vocab convertToVocab(JSONObject json) throws JSONException {
+		String primaryLanguageString = json.getString(PRIMARY_LANGUAGE);
+		SupportedLanguage primaryLanguage = SupportedLanguage.valueOf(primaryLanguageString.toUpperCase().trim());
+		System.out.println(primaryLanguage.name());
+		Vocab vocab = new Vocab(primaryLanguage);
+		for (SupportedLanguage language : SupportedLanguage.values()) {
+			if (json.has(language.toString())) {
+				JSONObject langJson = json.getJSONObject(language.toString());
+				vocab.addLanguage(language, langJson.getString(TRANSLATION));
+				vocab.setScore(language, langJson.getInt(TIMES_TESTED), langJson.getInt(TIMES_CORRECT));
 			}
-			return vocab;
-		} catch (JSONException e) {
-			e.printStackTrace();
-			return null;
 		}
-
+		return vocab;
 	}
 }
