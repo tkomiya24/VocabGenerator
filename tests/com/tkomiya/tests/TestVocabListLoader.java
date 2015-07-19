f128e5b645ac4b5032e7b406183d3d7ae57c6162
Takeru Komiya <tkomiya24@gmail.com>
2015-07-19 09:11:22 -0400

    Updates class to use the new SupportedLanguage enum

--- tests/com/tkomiya/tests/TestVocabListLoader.java
+++ tests/com/tkomiya/tests/TestVocabListLoader.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.tkomiya.models.Vocab;
+import com.tkomiya.models.Vocab.SupportedLanguage;
 import com.tkomiya.models.VocabList;
 
 public class TestVocabListLoader {
@@ -19,35 +20,35 @@ public class TestVocabListLoader {
 	}
 	
 	public static Vocab monday() {
-		Vocab monday = new Vocab(Vocab.ENGLISH);
-		monday.addLanguage(Vocab.ENGLISH, "Monday");
-		monday.setScore(Vocab.ENGLISH, 0, 0);
-		monday.addLanguage(Vocab.KOREAN, "월요일");
-		monday.setScore(Vocab.KOREAN, 1, 1);
-		monday.addLanguage(Vocab.JAPANESE, "月曜日");
-		monday.setScore(Vocab.JAPANESE, 2, 2);
+		Vocab monday = new Vocab(SupportedLanguage.ENGLISH);
+		monday.addLanguage(SupportedLanguage.ENGLISH, "Monday");
+		monday.setScore(SupportedLanguage.ENGLISH, 0, 0);
+		monday.addLanguage(SupportedLanguage.KOREAN, "월요일");
+		monday.setScore(SupportedLanguage.KOREAN, 1, 1);
+		monday.addLanguage(SupportedLanguage.JAPANESE, "月曜日");
+		monday.setScore(SupportedLanguage.JAPANESE, 2, 2);
 		return monday;
 	}
 	
 	public static Vocab tuesday() {
-		Vocab tuesday = new Vocab(Vocab.ENGLISH);
-		tuesday.addLanguage(Vocab.ENGLISH, "Tuesday");
-		tuesday.addLanguage(Vocab.KOREAN, "화요일");
-		tuesday.addLanguage(Vocab.JAPANESE, "火曜日");
-		tuesday.setScore(Vocab.ENGLISH, 4, 5);
-		tuesday.setScore(Vocab.KOREAN, 6, 14);
-		tuesday.setScore(Vocab.JAPANESE, 1, 7);
+		Vocab tuesday = new Vocab(SupportedLanguage.ENGLISH);
+		tuesday.addLanguage(SupportedLanguage.ENGLISH, "Tuesday");
+		tuesday.addLanguage(SupportedLanguage.KOREAN, "화요일");
+		tuesday.addLanguage(SupportedLanguage.JAPANESE, "火曜日");
+		tuesday.setScore(SupportedLanguage.ENGLISH, 4, 5);
+		tuesday.setScore(SupportedLanguage.KOREAN, 6, 14);
+		tuesday.setScore(SupportedLanguage.JAPANESE, 1, 7);
 		return tuesday;
 	}
 	
 	public static Vocab wednesday() {
-		Vocab wednesday = new Vocab(Vocab.ENGLISH);
-		wednesday.addLanguage(Vocab.ENGLISH, "Wednesday");
-		wednesday.addLanguage(Vocab.KOREAN, "수요일");
-		wednesday.addLanguage(Vocab.JAPANESE, "水曜日");
-		wednesday.setScore(Vocab.ENGLISH, 0, 0);
-		wednesday.setScore(Vocab.KOREAN, 1, 7);
-		wednesday.setScore(Vocab.JAPANESE, 9, 15);
+		Vocab wednesday = new Vocab(SupportedLanguage.ENGLISH);
+		wednesday.addLanguage(SupportedLanguage.ENGLISH, "Wednesday");
+		wednesday.addLanguage(SupportedLanguage.KOREAN, "수요일");
+		wednesday.addLanguage(SupportedLanguage.JAPANESE, "水曜日");
+		wednesday.setScore(SupportedLanguage.ENGLISH, 0, 0);
+		wednesday.setScore(SupportedLanguage.KOREAN, 1, 7);
+		wednesday.setScore(SupportedLanguage.JAPANESE, 9, 15);
 		return wednesday;
 	}
 	
