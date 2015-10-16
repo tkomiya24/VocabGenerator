3c918a3aeb391074fb5c319bf31ded2151fa1859
Takeru Komiya <tkomiya24@gmail.com>
2015-10-15 22:32:34 -0400

    Adds more values to the test data generator

--- tests/com/tkomiya/testutils/TestTranslationLoader.java
+++ tests/com/tkomiya/testutils/TestTranslationLoader.java
@@ -1,79 +1,122 @@
 package com.tkomiya.testutils;
 
+import java.util.Calendar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tkomiya.infrastructure.TranslationJsonConverter;
+import com.tkomiya.infrastructure.VocabJsonConverter;
 import com.tkomiya.main.Translation;
 
 public class TestTranslationLoader {
   
-  public Translation mondayEnglishTranslation() {
-    return new Translation().
-                setLastTested(null).
-                setTimesCorrect(0).
-                setTimesTested(0).
-                addTranslation("Monday");
+  public static Translation mondayEnglishTranslation() {
+    return makeTranslation("Monday", 0, 0);
   }
   
-  public Translation mondayKoreanTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(1).
-        setTimesTested(1).
-        addTranslation("월요일");
+  public static Translation mondayKoreanTranslation() {
+    return makeTranslation("월요일", 1, 1);
   }
   
-  public Translation mondayJapaneseTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(2).
-        setTimesTested(2).
-        addTranslation("月曜日");
+  public static Translation mondayJapaneseTranslation() {
+    return makeTranslation("月曜日", 2, 2);
   }
   
-  public Translation tuesdayEnglishTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(4).
-        setTimesTested(5).
-        addTranslation("Tuesday");
+  public static Translation tuesdayEnglishTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(1991, Calendar.JULY, 17, 6, 40, 44);
+    return makeTranslation("Tuesday", 4, 5, cal);
   }
   
-  public Translation tuesdayKoreanTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(6).
-        setTimesTested(14).
-        addTranslation("화요일");
+  public static Translation tuesdayKoreanTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(1991, Calendar.JULY, 17, 6, 40, 44);
+    return makeTranslation("화요일", 6, 14, cal);
   }
   
-  public Translation tuesdayJapaneseTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(1).
-        setTimesTested(7).
-        addTranslation("火曜日");
+  public static Translation tuesdayJapaneseTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(1991, Calendar.JULY, 17, 6, 40, 44);
+    return makeTranslation("火曜日", 1, 7, cal);
   }
   
-  public Translation wednesdayEnglishTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(0).
-        setTimesTested(0).
-        addTranslation("Wednesday");
+  public static Translation wednesdayEnglishTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(2015, Calendar.JANUARY, 15, 12, 15, 44);
+    return makeTranslation("Wednesday", 0, 0, cal);
   }
   
-  public Translation wednesdayKoreanTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(1).
-        setTimesTested(7).
-        addTranslation("수요일");
+  public static Translation wednesdayKoreanTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(2015, Calendar.JANUARY, 15, 12, 15, 44);
+    return makeTranslation("수요일", 1, 7, cal);
   }
   
-  public Translation wednesdayJapaneseTranslation() {
-    return new Translation().
-        setLastTested(null).
-        setTimesCorrect(9).
-        setTimesTested(15).
-        addTranslation("水曜日");
+  public static Translation wednesdayJapaneseTranslation() {
+    Calendar cal = Calendar.getInstance();
+    cal.clear();
+    cal.set(2015, Calendar.JANUARY, 15, 12, 15, 44);
+    return makeTranslation("水曜日", 9, 15, cal);
   }
   
+  public static JSONObject mondayEnglishJson() throws JSONException {
+    return makeTranslation("Monday", 0, 0, "");
+  }
+  
+  public static JSONObject mondayKoreanJson() throws JSONException {
+    return makeTranslation("월요일", 1, 1, "");
+  }
+
+  public static JSONObject mondayJapaneseJson() throws JSONException {
+    return makeTranslation("月曜日", 2, 2, "");
+  }
+  
+  public static JSONObject tuesdayEnglishJson() throws JSONException {
+    return makeTranslation("Tuesday", 4, 5, "1991-07-17T06:40:44.000Z");
+  }
+  
+  public static JSONObject tuesdayKoreanJson() throws JSONException {
+    return makeTranslation("화요일", 6, 14, "1991-07-17T06:40:44.000Z");
+  }
+
+  public static JSONObject tuesdayJapaneseJson() throws JSONException {
+    return makeTranslation("火曜日", 1, 7, "1991-07-17T06:40:44.000Z");  
+  }
+  
+  public static JSONObject wednesdayEnglishJson() throws JSONException {
+    return makeTranslation("Wednesday", 0, 0, "2015-01-15T12:15:44.000Z");
+  }
+  
+  public static JSONObject wednesdayKoreanJson() throws JSONException {
+    return makeTranslation("수요일", 1, 7, "2015-01-15T12:15:44.000Z");
+  }
+
+  public static JSONObject wednesdayJapaneseJson() throws JSONException {
+    return makeTranslation("水曜日", 9, 15, "2015-01-15T12:15:44.000Z");
+  }
+  
+  private static JSONObject makeTranslation(String translation, int timesCorrect, int timesTested, String lastTested) throws JSONException {
+    return new JSONObject().
+        put(TranslationJsonConverter.TRANSLATION, translation).
+        put(TranslationJsonConverter.TIMES_CORRECT, timesCorrect).
+        put(TranslationJsonConverter.TIMES_TESTED, timesTested).
+        put(TranslationJsonConverter.LAST_TESTED, lastTested);
+  }
+  
+  private static Translation makeTranslation(String translation, int timesCorrect, int timesTested) {
+    return new Translation().
+        setTimesCorrect(timesCorrect).
+        setTimesTested(timesTested).
+        addTranslation(translation);
+  }
+  
+  private static Translation makeTranslation(String translation, int timesCorrect, int timesTested, Calendar cal) {
+    return makeTranslation(translation, timesCorrect, timesTested).setLastTested(cal.getTime());
+  }
 }
