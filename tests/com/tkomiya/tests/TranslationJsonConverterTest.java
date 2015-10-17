35f43b3b0d60c9a9c572fff3b78f8ad21f9a1429
Takeru Komiya <tkomiya24@gmail.com>
2015-10-17 13:22:57 -0400

    Extracts methods to new class

--- tests/com/tkomiya/tests/TranslationJsonConverterTest.java
+++ tests/com/tkomiya/tests/TranslationJsonConverterTest.java
@@ -12,6 +12,7 @@ import com.tkomiya.infrastructure.TranslationJsonConverter;
 import com.tkomiya.testutils.TestTranslationLoader;
 import com.tkomiya.testutils.datageneration.translation.EnglishJsonFactory;
 import com.tkomiya.testutils.datageneration.translation.EnglishTranslationFactory;
+import com.tkomiya.testutils.datageneration.translation.JapaneseJsonFactory;
 import com.tkomiya.testutils.datageneration.translation.JapaneseTranslationFactory;
 import com.tkomiya.testutils.datageneration.translation.KoreanTranslationFactory;
 
@@ -21,26 +22,26 @@ public class TranslationJsonConverterTest {
   public void jsonToTranslation() throws JSONException, ParseException {
     assertEquals(EnglishTranslationFactory.mondayTranslation(), TranslationJsonConverter.convertToTranslation(EnglishJsonFactory.mondayEnglishJson()));
     assertEquals(KoreanTranslationFactory.mondayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.mondayKoreanJson()));
-    assertEquals(JapaneseTranslationFactory.mondayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.mondayJapaneseJson()));
+    assertEquals(JapaneseTranslationFactory.mondayTranslation(), TranslationJsonConverter.convertToTranslation(JapaneseJsonFactory.mondayJapaneseJson()));
     assertEquals(EnglishTranslationFactory.tuesdayTranslation(), TranslationJsonConverter.convertToTranslation(EnglishJsonFactory.tuesdayEnglishJson()));
     assertEquals(KoreanTranslationFactory.tuesdayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.tuesdayKoreanJson()));
-    assertEquals(JapaneseTranslationFactory.tuesdayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.tuesdayJapaneseJson()));
+    assertEquals(JapaneseTranslationFactory.tuesdayTranslation(), TranslationJsonConverter.convertToTranslation(JapaneseJsonFactory.tuesdayJapaneseJson()));
     assertEquals(EnglishTranslationFactory.wednesdayTranslation(), TranslationJsonConverter.convertToTranslation(EnglishJsonFactory.wednesdayEnglishJson()));
     assertEquals(KoreanTranslationFactory.wednesdayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.wednesdayKoreanJson()));
-    assertEquals(JapaneseTranslationFactory.wednesdayTranslation(), TranslationJsonConverter.convertToTranslation(TestTranslationLoader.wednesdayJapaneseJson()));
+    assertEquals(JapaneseTranslationFactory.wednesdayTranslation(), TranslationJsonConverter.convertToTranslation(JapaneseJsonFactory.wednesdayJapaneseJson()));
   }
 
   @Test
   public void translationToJson() throws JSONException {
     JSONAssert.assertEquals(EnglishJsonFactory.mondayEnglishJson(), TranslationJsonConverter.convertToJson(EnglishTranslationFactory.mondayTranslation()), false);
     JSONAssert.assertEquals(TestTranslationLoader.mondayKoreanJson(), TranslationJsonConverter.convertToJson(KoreanTranslationFactory.mondayTranslation()), false);
-    JSONAssert.assertEquals(TestTranslationLoader.mondayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.mondayTranslation()), false);
+    JSONAssert.assertEquals(JapaneseJsonFactory.mondayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.mondayTranslation()), false);
     JSONAssert.assertEquals(EnglishJsonFactory.tuesdayEnglishJson(), TranslationJsonConverter.convertToJson(EnglishTranslationFactory.tuesdayTranslation()), false);
     JSONAssert.assertEquals(TestTranslationLoader.tuesdayKoreanJson(), TranslationJsonConverter.convertToJson(KoreanTranslationFactory.tuesdayTranslation()), false);
-    JSONAssert.assertEquals(TestTranslationLoader.tuesdayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.tuesdayTranslation()), false);
+    JSONAssert.assertEquals(JapaneseJsonFactory.tuesdayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.tuesdayTranslation()), false);
     JSONAssert.assertEquals(EnglishJsonFactory.wednesdayEnglishJson(), TranslationJsonConverter.convertToJson(EnglishTranslationFactory.wednesdayTranslation()), false);
     JSONAssert.assertEquals(TestTranslationLoader.wednesdayKoreanJson(), TranslationJsonConverter.convertToJson(KoreanTranslationFactory.wednesdayTranslation()), false);
-    JSONAssert.assertEquals(TestTranslationLoader.wednesdayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.wednesdayTranslation()), false);
+    JSONAssert.assertEquals(JapaneseJsonFactory.wednesdayJapaneseJson(), TranslationJsonConverter.convertToJson(JapaneseTranslationFactory.wednesdayTranslation()), false);
   }
   
 }
--- tests/com/tkomiya/testutils/TestTranslationLoader.java
+++ tests/com/tkomiya/testutils/TestTranslationLoader.java
@@ -8,26 +8,13 @@ public class TestTranslationLoader {
   public static JSONObject mondayKoreanJson() throws JSONException {
     return TestTranslationLoaderUtil.makeTranslationJson("월요일", 1, 1, "");
   }
-
-  public static JSONObject mondayJapaneseJson() throws JSONException {
-    return TestTranslationLoaderUtil.makeTranslationJson("月曜日", 2, 2, "");
-  }
   
   public static JSONObject tuesdayKoreanJson() throws JSONException {
     return TestTranslationLoaderUtil.makeTranslationJson("화요일", 6, 14, "1991-07-17T06:40:44.000Z");
   }
 
-  public static JSONObject tuesdayJapaneseJson() throws JSONException {
-    return TestTranslationLoaderUtil.makeTranslationJson("火曜日", 1, 7, "1991-07-17T06:40:44.000Z");  
-  }
-
-  
   public static JSONObject wednesdayKoreanJson() throws JSONException {
     return TestTranslationLoaderUtil.makeTranslationJson("수요일", 1, 7, "2015-01-15T12:15:44.000Z");
-  }
-
-  public static JSONObject wednesdayJapaneseJson() throws JSONException {
-    return TestTranslationLoaderUtil.makeTranslationJson("水曜日", 9, 15, "2015-01-15T12:15:44.000Z");
   }
 
 }
