0a47faa214be00729f6b70fa1a244506c2030568
Takeru Komiya <tkomiya24@gmail.com>
2015-11-22 13:42:47 -0500

    Fixes unit tests

--- tests/com/tkomiya/tests/TranslationTest.java
+++ tests/com/tkomiya/tests/TranslationTest.java
@@ -1,11 +1,12 @@
 package com.tkomiya.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 
 import com.tkomiya.main.Translation;
